fe990d5604d63341a7bc6d697bdf262e1bc642b2
jmaster98 <jo-ash@procentrica.co.za>
2021-02-04 15:59:47 +0200

    added page 3

--- src/routes.go
+++ src/routes.go
@@ -4,4 +4,5 @@ package main
 func (s *Server) routes() {
 	s.router.HandleFunc("/assetflexvalcondition", s.handleGetAssetFlexValCondition()).Methods("GET")
 	s.router.HandleFunc("/portfolio", s.handleGetPortfolio()).Methods("GET")
+	s.router.HandleFunc("/yearreplacement", s.handleGetYearReplacement()).Methods("GET")
 }
