176083862a2fe6c9057b59eb6d69f026560b8adb
jmaster98 <jo-ash@procentrica.co.za>
2021-02-05 12:37:19 +0200

    page 5 and 6

--- src/handlers.go
+++ src/handlers.go
@@ -226,3 +226,149 @@ func (s *Server) handleGetYearReplacement() http.HandlerFunc {
 		w.Write(js)
 	}
 }
+
+func (s *Server) handleGetRenewalProfile() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handle Get Renewal profile has Been Called...")
+
+		nodeid := r.URL.Query().Get("nodeid")
+
+		//Check if no Node ID was provided in the URL
+
+		if nodeid == "" {
+			w.WriteHeader(500)
+			fmt.Fprint(w, "Functional Location ID not properly provided in URL")
+			fmt.Println("Functional Location ID not properly provided in URL")
+			return
+		}
+
+		//post to crud service
+		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/renewalprofile?nodeid=" + nodeid)
+
+		//check for response error of 500
+		if respErr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, respErr.Error())
+			fmt.Println("Error in communication with CRUD service endpoint for request to get Renewal profile")
+			return
+		}
+		if req.StatusCode != 200 {
+			w.WriteHeader(req.StatusCode)
+			fmt.Fprint(w, "Request to DB can't be completed...")
+			fmt.Println("Request to DB can't be completed...")
+		}
+		if req.StatusCode == 500 {
+			w.WriteHeader(500)
+			bodyBytes, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Fprintf(w, "An internal error has occured whilst trying to get Renewal profile"+bodyString)
+			fmt.Println("An internal error has occured whilst trying to get Renewal profile" + bodyString)
+			return
+		}
+
+		//close the request
+		defer req.Body.Close()
+
+		//create new response struct for JSON list
+		assetsList := []RenewalProfile{}
+
+		//decode request into decoder which converts to the struct
+		decoder := json.NewDecoder(req.Body)
+		err1 := decoder.Decode(&assetsList)
+		if err1 != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err1.Error())
+			fmt.Println("Error occured in decoding get assets response ")
+			return
+		}
+		//convert struct back to JSON.
+		js, jserr := json.Marshal(assetsList)
+		if jserr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, jserr.Error())
+			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
+			return
+		}
+
+		//return success back to Front-End user
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(js)
+	}
+}
+
+func (s *Server) handleGetRiskCriticality() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handle Get Risk Criticality has Been Called...")
+
+		nodeid := r.URL.Query().Get("nodeid")
+
+		//Check if no Node ID was provided in the URL
+
+		if nodeid == "" {
+			w.WriteHeader(500)
+			fmt.Fprint(w, "Functional Location ID not properly provided in URL")
+			fmt.Println("Functional Location ID not properly provided in URL")
+			return
+		}
+
+		//post to crud service
+		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/riskcriticality?nodeid=" + nodeid)
+
+		//check for response error of 500
+		if respErr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, respErr.Error())
+			fmt.Println("Error in communication with CRUD service endpoint for request to get Risk Criticality")
+			return
+		}
+		if req.StatusCode != 200 {
+			w.WriteHeader(req.StatusCode)
+			fmt.Fprint(w, "Request to DB can't be completed...")
+			fmt.Println("Request to DB can't be completed...")
+		}
+		if req.StatusCode == 500 {
+			w.WriteHeader(500)
+			bodyBytes, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Fprintf(w, "An internal error has occured whilst trying to get Risk Criticality"+bodyString)
+			fmt.Println("An internal error has occured whilst trying to get Risk Criticality" + bodyString)
+			return
+		}
+
+		//close the request
+		defer req.Body.Close()
+
+		//create new response struct for JSON list
+		assetsList := []RiskCriticality{}
+
+		//decode request into decoder which converts to the struct
+		decoder := json.NewDecoder(req.Body)
+		err1 := decoder.Decode(&assetsList)
+		if err1 != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err1.Error())
+			fmt.Println("Error occured in decoding get assets response ")
+			return
+		}
+		//convert struct back to JSON.
+		js, jserr := json.Marshal(assetsList)
+		if jserr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, jserr.Error())
+			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
+			return
+		}
+
+		//return success back to Front-End user
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(js)
+	}
+}
--- src/routes.go
+++ src/routes.go
@@ -5,4 +5,6 @@ func (s *Server) routes() {
 	s.router.HandleFunc("/assetflexvalcondition", s.handleGetAssetFlexValCondition()).Methods("GET")
 	s.router.HandleFunc("/portfolio", s.handleGetPortfolio()).Methods("GET")
 	s.router.HandleFunc("/yearreplacement", s.handleGetYearReplacement()).Methods("GET")
+	s.router.HandleFunc("/renewalprofile", s.handleGetRenewalProfile()).Methods("GET")
+	s.router.HandleFunc("/riskcriticality", s.handleGetRiskCriticality()).Methods("GET")
 }
